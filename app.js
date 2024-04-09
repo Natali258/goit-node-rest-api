3446872b556df5b9f74d348236ddc206cebd37cc
Natali258 <natalya.holyavko@gmail.com>
2024-04-09 16:19:59 +0300

    hw-03-node.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
-const DB_HOST =
-  "mongodb+srv://natali:4SF2EiufL0Gt1hYK@cluster0.o47dvcc.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log("Database connection successful"))
-  .catch((error) => console.log("error.message"));
+import "dotenv/config";
 
 import express from "express";
 import morgan from "morgan";
@@ -33,3 +27,18 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
 });
+
+const DB_HOST =
+  "mongodb+srv://natali:4SF2EiufL0Gt1hYK@cluster0.o47dvcc.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,24 +1,44 @@
 import express from "express";
 
 import contactsControllers from "../controllers/contactsControllers.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+  updateStatusContactSchema,
+} from "../schemas/contactsSchemas.js";
+import { isValidId } from "../helpers/isValidId.js";
+import { validateBody } from "../helpers/validateBody.js";
 const {
   getAllContacts,
   getOneContact,
   deleteContact,
   createContact,
   updateContact,
+  updateStatusContact,
 } = contactsControllers;
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", isValidId, getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", isValidId, deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(updateContactSchema),
+  updateContact
+);
+
+contactsRouter.patch(
+  "/:id",
+  isValidId,
+  validateBody(updateStatusContactSchema),
+  updateStatusContact
+);
 
 export default contactsRouter;
