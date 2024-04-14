074654d765573420376db4d9d368e170a7094749
Natali258 <natalya.holyavko@gmail.com>
2024-04-14 10:57:59 +0300

    hw-04-node.js-users-authenticate

---
+++ helpers/authenticate.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+
+import { HttpError } from "../helpers/HttpError.js";
+import { searchUser } from "../services/usersServices.js";
+
+const { JWT_SECRET } = process.env;
+
+export async function authenticate(req, _, next) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return next(HttpError(401, "Not authorized"));
+  }
+
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    return next(HttpError(401, "Not authorized"));
+  }
+
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await searchUser({ _id: id });
+
+    if (!user) {
+      return next(HttpError(401, "User not found"));
+    }
+
+    if (!user.token) {
+      return next(HttpError(401, "User already signout"));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(HttpError(401, error.message));
+  }
+}
