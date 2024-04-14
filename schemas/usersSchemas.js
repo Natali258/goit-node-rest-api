ef580e7ff35330af70457c75bfdae27ecb5cb15e
Natali258 <natalya.holyavko@gmail.com>
2024-04-14 19:01:36 +0300

    hw-04-node.js-router

---
+++ schemas/usersSchemas.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+export const signinUserSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const signupUserSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const updateSubscriptionSchema = Joi.object({
+  subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
