34de4eefcdc61efe6d5d5af79778256a2b81c89d
Natali258 <natalya.holyavko@gmail.com>
2024-04-15 13:57:28 +0300

    hw-06-node.js-password

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,20 +1,20 @@
 import nodemailer from "nodemailer";
 
-const { META_PASSWORD } = process.env;
+const { EMAIL, PASSWORD } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: "ppa@snow.io",
-    pass: "slbk fkpd rory ooha",
+    user: EMAIL,
+    pass: PASSWORD,
   },
 };
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 export const sendEmail = async (data) => {
-  const email = { ...data, from: "ppa@snow.io" };
+  const email = { ...data, from: EMAIL };
 
   await transport.sendMail(email);
 };
